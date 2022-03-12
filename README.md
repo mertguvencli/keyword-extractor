97632a4ab68fd3ba84bffbf86462d30daaa5597b
Mert Güvençli <guvenclimert@gmail.com>
2022-03-12 21:52:34 +0300

    related links added readme and demo app updated

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # trending-techs-in-data-science
+
+*This project aims to find "what are the trending techs on **Data Science** jobs?" using **NER**.*
+
+**Medium Post**
+[Finding tech trends on Data Scientist jobs using NER](https://medium.com/@mertguvencli/finding-tech-trends-on-data-scientist-jobs-using-ner-4988c7da87ee)
+
+**Demo App with Streamlit**
+[huggingface](https://huggingface.co/spaces/mertguvencli/trending-techs-on-data-science)
+
+**Dataset (sqlite)**
+[linkedin.db](https://drive.google.com/file/d/1HbdljV4zaf3dXd0Iwc82ulQsV30gmLV0/view?usp=sharing)
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from numpy import place
 import spacy
 import streamlit as st
 from spacy_streamlit import visualize_ner
@@ -11,14 +12,21 @@ st.write("""
 # Finding tech trends on Data Science jobs using NER. (DEMO)
 """)
 
+
 nlp = spacy.load("en_core_web_sm")
 ruler = nlp.add_pipe("entity_ruler", before="ner")
 ruler.from_disk("data/patterns.jsonl")
 
-txt = st.text_area(label='Enter the job description')
-doc = nlp(txt)
+
+with open('data/sample_description.txt', 'r') as f:
+    description = f.read()
+
+text = st.text_area(label='Job Description', value=description, placeholder='Please enter a job description')
+
+doc = nlp(text)
 visualize_ner(doc, labels=nlp.get_pipe("ner").labels)
 
+st.subheader('Skill Requirements')
 st.json(
     [{'label': entity.label_, 'text': entity.text, 'start': entity.start, 'end': entity.end} \
         for entity in doc.ents if entity.ent_id_ == 'SKILLS']
